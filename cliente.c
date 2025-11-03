73222f9eca23f48c859fd9171ce83cc4081bc565
furet <154884652+fur3t@users.noreply.github.com>
2025-11-03 17:05:23 +0000

    cliente.c com comentários FIFO feito

--- cliente.c
+++ cliente.c
@@ -3,20 +3,20 @@
 #include <fcntl.h>
 #include <string.h>
 
-#define FIFO_CLIENTE "/tmp/fifo_cliente"
+#define FIFO_CLIENTE "/tmp/fifo_cliente" //caminho do FIFO do cliente
 
-void enviar_comando(const char *mensagem) {
-    int fd = open(FIFO_CLIENTE, O_WRONLY);
-    if (fd < 0) {
-        perror("Cliente: Erro ao abrir FIFO");
+void enviar_comando(const char *mensagem) { //função para enviar comando ao controlador
+    int fd = open(FIFO_CLIENTE, O_WRONLY); // abre FIFO para escrever
+    if (fd < 0) { // se der erro ao abrir o FIFO
+        perror("Cliente: Erro ao abrir FIFO"); //mostra que deu erro
         return;
     }
-    write(fd, mensagem, strlen(mensagem) + 1); // inclui o terminador '\0'
-    close(fd);
+    write(fd, mensagem, strlen(mensagem) + 1); // inclui o '\0'
+    close(fd); // fecha o FIFO
 }
 
 int main(int argc, char *argv[]) {
-    printf("Ola. A enviar comando de teste...\n");
-    enviar_comando("AGENDAR 12 5km LISBOA\n");
+    printf("Ola. A enviar comando de teste...\n"); //mensagem de teste
+    enviar_comando("AGENDAR 12 5km LISBOA\n"); //envia comando de agendar
     return 0;
 }
